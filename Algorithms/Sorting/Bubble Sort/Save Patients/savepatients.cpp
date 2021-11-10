e7361183515e903c2348bd0b1ca25970c8b4239f
Manasi Patil <pmanasi02@gmail.com>
2021-11-10 13:17:42 +0530

    Create savepatients.cpp

---
+++ Algorithms/Sorting/Bubble Sort/Save Patients/savepatients.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n; cin>>n;
+	int vacc[n],midcnt[n];
+	for(int i=0;i<n;i++) cin>>vacc[i];
+	for(int i=0;i<n;i++) cin>>midcnt[i];
+	bool ok=1;
+	for(int i=0;i<n;i++) if(vacc[i]<=midcnt[i]) ok=0;
+	if(ok) cout<<"Yes\n";
+	else cout<<"No\n";
+}
